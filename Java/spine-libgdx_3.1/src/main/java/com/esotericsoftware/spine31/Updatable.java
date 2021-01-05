6e404fa12ff15202df2e4aefbf4414aa1be93a15
Aloento <11802769+soarteam@users.noreply.github.com>
2021-01-05 19:34:14 +0100

    删除运行库

--- Java/spine-libgdx_3.1/src/main/java/com/esotericsoftware/spine31/Updatable.java
+++
@@ -1,6 +0,0 @@
-
-package com.esotericsoftware.spine31;
-
-public interface Updatable {
-	public void update ();
-}
--- Java/spine-libgdx_3.1/src/main/java/com/esotericsoftware/spine31/attachments/FfdAttachment.java
+++
@@ -1,6 +0,0 @@
-
-package com.esotericsoftware.spine31.attachments;
-
-public interface FfdAttachment {
-	public boolean applyFFD (Attachment sourceAttachment);
-}
